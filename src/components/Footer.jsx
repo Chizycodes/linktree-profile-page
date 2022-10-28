a06ec932e2d4e2b79343200b1e456fb685554e50
Joy Okwudire <okwudirejoy@gmail.com>
2022-10-28 07:39:06 +0000

    Add missing alt attributes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,9 @@ const Footer = () => {
   return (
     <footer className="mt-[100px] mx-[16px]">
       <div className="flex flex-col sm:flex-row sm:justify-between sm:items-center max-w-[750px] w-full mx-auto gap-[16px] border-t border-dashed py-3 ">
-        <a href="https://internship.zuri.team/" target="_blank"><img src="/zuri-logo.svg" alt=""/></a>
+        <a href="https://internship.zuri.team/" target="_blank"><img src="/zuri-logo.svg" alt="zuri logo"/></a>
         <p className="text-[#667085] text-base">HNG Internship 9 Frontend Task</p>
-        <a href="https://ingressive.org/" target="_blank"><img src="/I4g-logo.svg" alt=""/></a>
+        <a href="https://ingressive.org/" target="_blank"><img src="/I4g-logo.svg" alt="I4G logo"/></a>
       </div>
       
     </footer>

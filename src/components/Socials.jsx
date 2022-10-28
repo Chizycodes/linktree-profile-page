20533445d7439ff6b561957f6c6b70aeeb424231
Joy Okwudire <okwudirejoy@gmail.com>
2022-10-28 10:10:11 +0000

    commit

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -5,8 +5,6 @@ const Socials = () => {
     <div className="flex justify-center mt-[24px] gap-3">
       <a href="https://hng9.slack.com" target="_blank"> <img src="/slack.svg" alt="slack" /></a>
       <a href="https://github/Chizycodes" target="_blank"><img src="/github.svg" alt="github" /></a>
-
-
     </div>
   )
 }

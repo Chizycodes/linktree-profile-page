0091e0f6fd54b6133a381c635dc860f162c31230
Joy Okwudire <okwudirejoy@gmail.com>
2022-10-28 10:09:00 +0000

    minor change

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 const Socials = () => {
   return (
     <div className="flex justify-center mt-[24px] gap-3">
-      <a href="" target="_blank"> <img src="/slack.svg" alt="slack" /></a>
+      <a href="https://hng9.slack.com" target="_blank"> <img src="/slack.svg" alt="slack" /></a>
       <a href="https://github/Chizycodes" target="_blank"><img src="/github.svg" alt="github" /></a>
 
 

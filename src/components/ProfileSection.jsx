16bff4efb1b180d49ad59b73a09a0057492b4b7a
Joy Okwudire <okwudirejoy@gmail.com>
2022-10-28 06:06:13 +0000

    modification

--- src/components/ProfileSection.jsx
+++ src/components/ProfileSection.jsx
@@ -4,7 +4,7 @@ const ProfileSection = () => {
   const [overlay, setOverlay] = useState(false);
   return (
     <div className="relative w-full px-[16px] mx-auto max-w-[700px] ">
-      <div className="w-[40px] h-[40px] rounded-full border border-[#D0D5DD] flex items-center justify-center absolute right-[16px] sm:right-[80px] top-[-20px] cursor-pointer"><img src="/share-icon.svg" alt="share-icon" className="hidden sm:block"/><img src="/dots-icon.svg" alt="share-icon" className="sm:hidden"/></div>
+      <div className="w-[40px] h-[40px] rounded-full border border-dashed border-[#D0D5DD] flex items-center justify-center absolute right-[16px] sm:right-[80px] top-[-20px] cursor-pointer hover:bg-[#D0D5DD]"><img src="/share-icon.svg" alt="share-icon" className="hidden sm:block"/><img src="/dots-icon.svg" alt="share-icon" className="sm:hidden"/></div>
     <div className="flex flex-col items-center">
       <div className="relative w-[88px] h-[88px] rounded-full cursor-pointer mb-[24px]" onMouseEnter={()=>setOverlay(true)} onMouseLeave={()=>setOverlay(false)}>
         <img src="/dp.jpg" id="profile__img" className="w-[88px] h-[88px] rounded-full" alt="my photo"/> 

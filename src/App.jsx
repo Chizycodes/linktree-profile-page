d379f3ee00bde4d9ae9dc89496ea8cd2c6030900
Joy Okwudire <okwudirejoy@gmail.com>
2022-11-03 15:06:07 +0000

    fix: footer alignment

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ export default function App() {
         <Route path='*' element={<Home />} />
       </Routes>
 
-      <section>
+      <section className='w-full'>
         <Footer />
       </section>
     </div>

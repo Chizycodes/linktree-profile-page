beaa54b07bf30f8ca0e64b8e07958aa05a2d7679
Joy Okwudire <okwudirejoy@gmail.com>
2022-11-03 15:41:22 +0000

    fix: added required ids

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -7,24 +7,24 @@ const Contact = () => {
       <form className="w-full">
         <div className="grid grid-cols-12 gap-[24px] w-full mb-[24px]">
           <div className="flex flex-col col-span-12 md:col-span-6">
-            <label className="text-[#344054]">First name</label>
-            <input type="text" placeholder="Enter your first name" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
+            <label className="text-[#344054]" htmlFor="first_name">First name</label>
+            <input type="text" id="first_name" placeholder="Enter your first name" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
           </div>
 
           <div className="flex flex-col col-span-12 md:col-span-6">
-            <label className="text-[#344054]">Last name</label>
-            <input type="text" placeholder="Enter your last name" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
+            <label className="text-[#344054]" htmlFor="last_name">Last name</label>
+            <input type="text" id="last_name" placeholder="Enter your last name" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
           </div>
         </div>
 
         <div className="flex flex-col col-span-12 md:col-span-6 mb-[24px]">
-          <label className="text-[#344054]">Email</label>
-          <input type="text" placeholder="yourname@email.com" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
+          <label className="text-[#344054]" htmlFor="email">Email</label>
+          <input type="text" id="email" placeholder="yourname@email.com" className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none" />
         </div>
 
         <div className="flex flex-col col-span-12 md:col-span-6 mb-[24px]">
-          <label className="text-[#344054]">Message</label>
-          <textarea type="text" placeholder="Send me a message and I'll reply you as soon as possible..." className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none h-[132px]" />
+          <label className="text-[#344054]" htmlFor="message">Message</label>
+          <textarea type="text" id="message" placeholder="Send me a message and I'll reply you as soon as possible..." className="border border-[#D0D5DD] rounded-[8px] py-[10px] px-[14px] mt-[6px] focus:border-[#84CAFF] outline-none h-[132px]" />
         </div>
 
         <div className="flex gap-2 mb-[32px]">
@@ -32,7 +32,7 @@ const Contact = () => {
           <label className="text-[#475467]">You agree to providing your data to Joy who may contact you.</label>
         </div>
 
-        <button className="text-white bg-[#1570EF] py-[12px] rounded-[8px] w-full">
+        <button className="text-white bg-[#1570EF] py-[12px] rounded-[8px] w-full" id="btn__submit">
           Send message
         </button>
 
